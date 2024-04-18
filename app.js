14a59fc024ad9deeb58b61cf65f5509109391ce3
Aravindh Balaji <142385502+aravindhbalaji04@users.noreply.github.com>
2024-04-19 04:14:21 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+const arrows = document.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll(".movie-list");
+
+arrows.forEach((arrow, i) => {
+    const itemNumber = movieLists[i].querySelectorAll("img").length;
+    let clickCounter = 0;
+
+    arrow.addEventListener("click", () => {
+        clickCounter++;
+        if (itemNumber - (3 + clickCounter) >= 0) {
+            movieLists[i].style.transform = `translateX(${
+          movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
+        }px)`;
+        } else {
+            movieLists[i].style.transform = "translateX(0)";
+            clickCounter = 0;
+        }
+    });
+    console.log(movieLists[i].querySelectorAll("img").length)
+})
